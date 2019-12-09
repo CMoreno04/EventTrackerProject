ed517a3ff8bd15dd4ec893b6940c8de3eac8321f
CMoreno04 <cesar.jos.em26@icloud.com>
2019-12-08 17:07:02 -0700

    adds frontend

---
+++ EventTracker/src/main/resources/static/js/script.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', function(e){
+	console.log("Document Loaded")
+	init();
+});
+
+function init() {
+	console.log("in init");
+	
+document.form.submit.addEventListener('click',function(e) {
+	console.log('submit');
+	
+})	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}

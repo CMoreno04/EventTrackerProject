43c2b072b1ac0287b0dab7a9edde7a6476d74dd2
CMoreno04 <cesar.jos.em26@icloud.com>
2019-12-15 12:46:03 -0700

    adds flightracker

--- EventTracker/src/main/java/com/skilldistillery/eventtracker/repository/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skilldistillery.eventtracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.skilldistillery.eventtracker.entities.Flight;
-
-public interface FlightRepository extends JpaRepository<Flight, Integer>{
-
-}
